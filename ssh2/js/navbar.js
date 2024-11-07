ef9c260f92470da3394278aaee3823aad7e676b2
M . H . Senvith <163434987+Senvith@users.noreply.github.com>
2024-11-07 17:06:32 +0530

    Delete ssh2/js directory

--- ssh2/js/navbar.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('menu-icon').addEventListener('click', function() {
-    const navLinks = document.getElementById('nav-links');
-    navLinks.classList.toggle('show'); // Toggle the "show" class
-});
